227d19343127cd30fc8b84aa7f56b1e01f308e3a
Alejandro@BEELZEBUB <alejandro@evotechnology.co>
2020-04-16 20:58:13 -0400

    firebase  and fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  baseUrl: '', // <Your API base url>
-  imageUrl: '', // <Your API url for image resize>
-  productUrl: '' // <Your store base url>
+  baseUrl: 'https://api.decasta.com.ve/api', // <Your API base url>
+  imageUrl: 'https://api.decasta.com.ve/api/media/image-resize', // <Your API url for image resize>
+  productUrl: 'https://store.decasta.com.ve' // <Your store base url>
 
 };
