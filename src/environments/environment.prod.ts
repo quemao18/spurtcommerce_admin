5ac08923eb5fcc66db617f909ce98187df99e791
Alejandro@BEELZEBUB <alejandro@evotechnology.co>
2020-04-17 22:34:33 -0400

    fix route

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   baseUrl: 'https://api.decasta.com.ve/api', // <Your API base url>
   imageUrl: 'https://api.decasta.com.ve/api/media/image-resize', // <Your API url for image resize>
-  productUrl: 'https://store.decasta.com.ve' // <Your store base url>
+  productUrl: 'https://store.decasta.com.ve/' // <Your store base url>
 
 };
